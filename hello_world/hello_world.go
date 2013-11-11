f699aed8f5011bc7c5f9d1afaa368f5a074dad6d
Zhao Ximo <zhaoximo@hotmail.com>
2013-11-11 18:04:20 +0800

    my first go hello world

---
+++ hello_world/hello_world.go
@@ -0,0 +1,19 @@
+//这个文件演示了hello world
+
+package main
+
+import ("fmt"
+	
+	)
+
+func main(){
+	//who := "World"
+	//if len(os.Args) > 1{
+	//who = strings.Join(os.Args[1:]," ")
+	//fmt.Println("Hello ",who)
+	//}
+	fmt.Println("hello world");
+
+}
+
+
